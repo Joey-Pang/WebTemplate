08cb2ff4ae28f993270d3e00695b791938643450
kar_kit_ <joeypangster@gmail.com>
2022-03-07 19:16:30 +0000

    Sanity and Frontend Skeleton Created

---
+++ frontend_react/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReacDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+
+//Connects the whole app to div in index.html
+ReacDOM.render(<App />, document.getElementById("root"));
