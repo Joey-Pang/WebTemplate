851fcebc668cc741f5a2d7c466539e4a1eceed24
kar_kit_ <joeypangster@gmail.com>
2022-03-08 18:20:59 +0000

    Image js added

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Navbar.scss";
+import images from "../../assets/tvb";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src="{ images }" alt="logo" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,3 @@
+import logo from "../assets/tvb.jpg";
+
+export default { logo };
