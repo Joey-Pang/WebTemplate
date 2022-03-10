4a2c6667800e649ed9d731dcb4d7c4fd91eea7e0
kar_kit_ <joeypangster@gmail.com>
2022-03-10 06:42:13 +0000

    Hambubrger menu built and functional

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       {/* BEM methodology naming message */}
@@ -20,6 +24,29 @@ const Navbar = () => {
           )
         )}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className="app__navbar-links">
+              {["home", "about", "contact", "work", "skills", "contact"].map(
+                (item) => (
+                  <li key={{ item }}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
